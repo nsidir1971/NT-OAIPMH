eaedcbad1bcf59f106cfe050bd3e33951d320e19
nsidir <nsidir@athenarc.gr>
2024-11-26 19:15:38 +0200

    first verbs

--- class.php
+++ class.php
@@ -4,17 +4,26 @@ global $semanticTypes;
 global $repo_url, $handlerURL;
 
 $semanticTypes=array(
-    'program' => array('labelGR'=>'Πρόγραμμα','labelEN'=>'Program','concept' => 'https://semantics.gr/authorities/ekt-item-types/Theatrical-program', 'exact'=> 'http://vocab.getty.edu/page/aat/300027217'),
-    'pub' => array('labelGR'=>'Δημοσίευμα','labelEN'=>'Publication','concept'=>'https://semantics.gr/authorities/EKTrepository-resourceTypes/1017094458', 'exact'=>'http://vocab.getty.edu/page/aat/300111999'),
-    'photo' => array('labelGR'=>'Φωτογραφία','labelEN'=>'Photograph','concept'=>'https://semantics.gr/authorities/ekt-unesco/1476367459', 'exact'=> 'https://vocabularies.unesco.org/thesaurus/concept11166'),
-    'histphoto' => array('labelGR'=>'Ιστορική φωτογραφία','labelEN'=>'Historical Photograph','concept'=>'https://semantics.gr/authorities/ekt-unesco/1476367459', 'exact'=> 'https://vocabularies.unesco.org/thesaurus/concept11166'),
-    'sound' => array('labelGR'=>'Ηχογράφιση','labelEN'=>'Sound Recording','concept'=>'https://semantics.gr/authorities/EKTrepository-resourceTypes/541140624', 'exact'=>'https://vocabularies.unesco.org/thesaurus/concept9812'),
-    'soundpart' => array('labelGR'=>'Ηχογράφιση','labelEN'=>'Sound Recording','concept'=>'https://semantics.gr/authorities/EKTrepository-resourceTypes/541140624', 'exact'=>'https://vocabularies.unesco.org/thesaurus/concept9812'),
-    'video' => array('labelGR'=>'Μαγνητοσκόπηση','labelEN'=>'Video Recording','concept'=>'https://semantics.gr/authorities/SSH-LCSH/sh2008000037', 'exact'=>'https://id.loc.gov/authorities/subjects/sh2008000037'),
-    'videopart' => array('labelGR'=>'Μαγνητοσκόπηση','labelEN'=>'Video Recording','concept'=>'http://semantics.gr/authorities/SSH-LCSH/sh2008000037', 'exact'=>'https://id.loc.gov/authorities/subjects/sh2008000037'),
-    'poster' => array('labelGR'=>'Αφίσα','labelEN'=>'Poster','concept'=>'https://semantics.gr/authorities/ekt-item-types/afisa', 'exact'=>'https://vocabularies.unesco.org/thesaurus/concept1604'),
-    'costume' => array('labelGR'=>'Θεατρικό κοστούμι','labelEN'=>'Costume','concept'=>'https://semantics.gr/authorities/craft-item-types/941945856', 'exact'=>'https://vocabularies.unesco.org/thesaurus/concept5474'),
-    'musicScore' => array('labelGR'=>'Παρτιτούρα','labelEN'=>'Music Score','concept'=>'https://semantics.gr/authorities/openarchives-item-types/Score', 'exact'=>'https://vocab.getty.edu/page/aat/300111999')
+    'program' => array('labelGR'=>'Πρόγραμμα','labelEN'=>'Program','concept' => 'https://semantics.gr/authorities/ekt-item-types/Theatrical-program', 'exact'=> 'http://vocab.getty.edu/page/aat/300027217', 
+                    'set'=>'programs' ,'setName'=>'Προγράμματα','setDescription'=>'Συλλογή προγραμμάτων από θεατρικές παραστάσεις', 'setDescriptionEN'=>'Collection of programs related to theatrical plays'),
+    'pub' => array('labelGR'=>'Δημοσίευμα','labelEN'=>'Publication','concept'=>'https://semantics.gr/authorities/EKTrepository-resourceTypes/1017094458', 'exact'=>'http://vocab.getty.edu/page/aat/300111999', 
+                    'set'=>'pubs' ,'setName'=>'Δημοσιεύματα','setDescription'=>'Συλλογή δημοσιευμάτων για θεατρικές παραστάσεις', 'setDescriptionEN'=>'Collection of publications related to theatrical plays'),
+    'photo' => array('labelGR'=>'Φωτογραφία','labelEN'=>'Photograph','concept'=>'https://semantics.gr/authorities/ekt-unesco/1476367459', 'exact'=> 'https://vocabularies.unesco.org/thesaurus/concept11166',
+                    'set'=>'photos' ,'setName'=>'Φωτογραφίες','setDescription'=>'Συλλογή φωτογραφιών από θεατρικές παραστάσεις', 'setDescriptionEN'=>'Collection of photos from theatrical plays'),
+    'histphoto' => array('labelGR'=>'Ιστορική φωτογραφία','labelEN'=>'Historical Photograph','concept'=>'https://semantics.gr/authorities/ekt-unesco/1476367459', 'exact'=> 'https://vocabularies.unesco.org/thesaurus/concept11166',
+                    'set'=>'histphotos' ,'setName'=>'Ιστορικές Φωτογραφίες','setDescription'=>'Συλλογή φωτογραφιών από στιγμές που σχετίζονται με την ιστορία του Εθνικού Θεάτρου', 'setDescriptionEN'=>'Collection of photos documenting momments from the history of National Theatre of Greece'),
+    'sound' => array('labelGR'=>'Ηχογράφιση','labelEN'=>'Sound Recording','concept'=>'https://semantics.gr/authorities/EKTrepository-resourceTypes/541140624', 'exact'=>'https://vocabularies.unesco.org/thesaurus/concept9812',
+                    'set'=>'sounds' ,'setName'=>'Ηχογραφήσεις','setDescription'=>'Συλλογή ηχογραφήσεων από θεατρικές παραστάσεις', 'setDescriptionEN'=>'Collection of sound recordings from theatrical plays'),
+    'soundpart' => array('labelGR'=>'Ηχογράφιση','labelEN'=>'Sound Recording','concept'=>'https://semantics.gr/authorities/EKTrepository-resourceTypes/541140624', 'exact'=>'https://vocabularies.unesco.org/thesaurus/concept9812',
+                    'set'=>'soundparts' ,'setName'=>'Αποσπάσματα ηχογραφήσεων','setDescription'=>'Συλλογή αποσπασμάτων ηχογραφήσεων από θεατρικές παραστάσεις', 'setDescriptionEN'=>'Collection of parts of sound recordings from theatrical plays'),
+    'video' => array('labelGR'=>'Μαγνητοσκόπηση','labelEN'=>'Video Recording','concept'=>'https://semantics.gr/authorities/SSH-LCSH/sh2008000037', 'exact'=>'https://id.loc.gov/authorities/subjects/sh2008000037',
+                    'set'=>'videos' ,'setName'=>'Βίντεο','setDescription'=>'Συλλογή μαγνητοσκοπήσεων θεατρικών παραστάσεων', 'setDescriptionEN'=>'Collection of video recordings from theatrical plays'),
+    'videopart' => array('labelGR'=>'Μαγνητοσκόπηση','labelEN'=>'Video Recording','concept'=>'http://semantics.gr/authorities/SSH-LCSH/sh2008000037', 'exact'=>'https://id.loc.gov/authorities/subjects/sh2008000037',
+                    'set'=>'videoparts' ,'setName'=>'Αποσπάσματα μαγνητοσκοπήσεων','setDescription'=>'Συλλογή αποσπασμάτων μαγνητοσκοπήσεων από θεατρικές παραστάσεις', 'setDescriptionEN'=>'Collection of parts of video recordings from theatrical plays'),
+    'poster' => array('labelGR'=>'Αφίσα','labelEN'=>'Poster','concept'=>'https://semantics.gr/authorities/ekt-item-types/afisa', 'exact'=>'https://vocabularies.unesco.org/thesaurus/concept1604',
+                    'set'=>'posters' ,'setName'=>'Αφίσες','setDescription'=>'Συλλογή αφισών από θεατρικές παραστάσεις', 'setDescriptionEN'=>'Collection of posters from theatrical plays'),
+    'costume' => array('labelGR'=>'Θεατρικό κοστούμι','labelEN'=>'Costume','concept'=>'https://semantics.gr/authorities/craft-item-types/941945856', 'exact'=>'https://vocabularies.unesco.org/thesaurus/concept5474',
+                    'set'=>'costumes' ,'setName'=>'Κοστούμια','setDescription'=>'Συλλογή θεατρικών κοστουμιών από παραστάσεις', 'setDescriptionEN'=>'Collection of theatrical costumes from plays')
 );
 
 ini_set('memory_limit', '-1');
@@ -46,6 +55,84 @@ function validatemetaPref($param){
         case 'edm':
             $res = true;
             break;
+    }
+    return $res;
+}
+
+function validateSet($set){
+    global $semanticTypes;
+    $res = false;
+
+    if(array_key_exists($set, $semanticTypes)){
+        $res = true;
+    }
+    return $res;
+}
+
+function getIdentifiers($from, $until, $set){
+    global $dbh;
+    $res = array();
+    $sqlwhere = '';
+    if($from!=''){
+        $sqlwhere = "WHERE timestamp>='$from' ";
+    }
+
+    if($until != ''){
+        if($from != ''){
+            $sqlwhere .= " AND timestamp<='$until'";
+        }else{
+            $sqlwhere = "WHERE timestamp<='$until' ";
+        }
+
+    }
+
+    if($set != ''){ //selected collection
+        if($sqlwhere != ''){
+            $sqlwhere .= " AND setgroup='$set' ";
+        }else{
+            $sqlwhere = "WHERE setgroup='$set' ";
+        }
+        
+    }
+    $sql = "WITH ds AS (
+                select programID AS itemID, (CASE WHEN modified IS NOT NULL THEN modified ELSE created END) AS timestamp, 'program' AS setgroup
+                FROM programs WHERE published=1
+                UNION
+                select pubID AS itemID, (CASE WHEN modified IS NOT NULL THEN modified ELSE created END) AS timestamp, 'publication' AS setgroup
+                FROM publications WHERE published=1
+                UNION
+                select photoID AS itemID, (CASE WHEN modified IS NOT NULL THEN modified ELSE created END) AS timestamp, 'photo' AS setgroup
+                FROM photos WHERE published=1
+                UNION
+                select histphotoID AS itemID, (CASE WHEN modified IS NOT NULL THEN modified ELSE created END) AS timestamp, 'histphoto' AS setgroup
+                FROM historicPhotos WHERE published=1
+                UNION
+                select soundID AS itemID, (CASE WHEN modified IS NOT NULL THEN modified ELSE created END) AS timestamp, 'sound' AS setgroup
+                FROM sounds WHERE published=1
+                UNION
+                select soundpartID AS itemID, (CASE WHEN modified IS NOT NULL THEN modified ELSE created END) AS timestamp, 'soundspart' AS setgroup
+                FROM soundParts WHERE published=1
+                UNION
+                select videoID AS itemID, (CASE WHEN modified IS NOT NULL THEN modified ELSE created END) AS timestamp, 'video' AS setgroup
+                FROM videos WHERE published=1
+                UNION
+                select videopartID AS itemID, (CASE WHEN modified IS NOT NULL THEN modified ELSE created END) AS timestamp, 'videopart' AS setgroup
+                FROM videoParts WHERE published=1
+                UNION
+                select posterID AS itemID, (CASE WHEN modified IS NOT NULL THEN modified ELSE created END) AS timestamp, 'poster' AS setgroup
+                FROM posters WHERE published=1
+                UNION
+                select costumeID AS itemID, (CASE WHEN modified IS NOT NULL THEN modified ELSE created END) AS timestamp, 'costume' AS setgroup
+                FROM costumes WHERE published=1
+                )
+            select * from ds $sqlwhere
+            ORDER BY  timestamp, setgroup";
+    try{
+        $stmt=$dbh->query($sql) or die();
+        $res=$stmt->fetchAll(PDO::FETCH_ASSOC);
+
+    }catch(PDOException $err){
+        echo $err->getMessage();
     }
     return $res;
 }
--- index.php
+++ index.php
@@ -1,10 +1,15 @@
 <?php
 global $dbh;
 global $oaiURL;
+global $semanticTypes;
 include_once 'class.php';
 $oaiURL = 'https://www.nt-archive.gr/oai-pmh';
 $error = '';
 $hasrequestError = false;
+
+//DB connection
+$dbh = db_connect();
+
 
 //verb parameter
 $verb='';
@@ -30,9 +35,11 @@ if(isset($_GET['metadataPrefix']) and $_GET['metadataPrefix'] != ''){
 
 //from param
 $from='';
+$fromtext = '';
 if(isset($_GET['from']) and $_GET['from'] != ''){
     if(validateDate($_GET['from'])){
         $from = $_GET['from'];
+        $fromtext = ' from="' . $from . '"';
     }else{
         $error = 'badArgument';
         $hasrequestError = true;
@@ -40,9 +47,11 @@ if(isset($_GET['from']) and $_GET['from'] != ''){
 }
 // until param
 $until = '';
+$untiltext='';
 if(isset($_GET['until']) and $_GET['until'] != ''){
     if(validateDate($_GET['until'])){
         $until = $_GET['until'];
+        $untiltext= ' until="' . $until . '"';
     }else{
         $error = 'badArgument';
         $hasrequestError = true;
@@ -51,14 +60,22 @@ if(isset($_GET['until']) and $_GET['until'] != ''){
 
 //set param
 $set = '';
+$settext='';
 if(isset($_GET['set']) and $_GET['set']!=''){
     $set = $_GET['set'];
+    if(!validateSet($set)){
+        $error = 'badArgument';
+        $hasrequestError = true;
+    }
+    $settext = ' set="' . $set . '"';
 }
 
 //resumptionToken param
 $resumptionToken ='';
+$restext='';
 if(isset($_GET['resumptionToken']) and $_GET['resumptionToken']!='' ){
     $resumptionToken = $_GET['resumptionToken'];
+    $restext = ' resumptionToken="' . $resumptionToken . '"';
 
 }
 
@@ -96,14 +113,97 @@ switch($verb){
                     </Identify>';
         break;
     case "ListMetadataFormats":
+        $xmlRequest = '<request verb="ListMetadataFormats">' . $oaiURL . '</request>';
+        $xmlBody = '<ListMetadataFormats>
+                        <metadataFormat>
+                            <metadataPrefix>edm</metadataPrefix>
+                            <schema>http://www.europeana.eu/schemas/edm/EDM.xsd</schema>
+                            <metadataNamespace>http://www.europeana.eu/schemas/edm/</metadataNamespace>
+                        </metadataFormat>
+                    </ListMetadataFormats>'; 
         break;
     case "GetRecord":
         break;
     case "ListRecords":
         break;
     case "ListSets":
+        $xmlRequest = '<request verb="ListSets">' . $oaiURL . '</request>';
+        $xmlBody = '<listSets>';
+        $xmlset = '';
+        foreach($semanticTypes as $key => $set){
+
+            $xmlset .= '<set>
+                            <setSpec>' . $set['set'] . '</setSpec>
+                            <setName>' . $set['setName'] . '</setName>
+                            <setDescription >
+                                <dc:description xmlns:dc="http://purl.org/dc/elements/1.1/" xml:lang="en">'.
+                                $set['setDescription'] .
+                                '</dc:description>
+                                <dc:description xmlns:dc="http://purl.org/dc/elements/1.1/" xml:lang="el">'.
+                                $set['setDescriptionEN'] .
+                                '</dc:description>
+                            </setDescription>
+                       </set>';
+
+        }
+        $xmlBody .= $xmlset . '</listSets>';
         break;
     case "ListIdentifiers":
+        $cursor = '';
+
+        $xmlRequest = '<request verb="ListIdentifiers" metadataPrefix="' . $metadataPrefix . '" ' . $settext . $fromtext . $untiltext . $restext . '>' . $oaiURL . '</request>';
+        $allIdentifiers = getIdentifiers($from, $until, $set);
+        $currdate = date('YYYYMMDD');
+        if(count($allIdentifiers) > 100){
+            if($resumptionToken != ''){
+                $decodedresumption = base64_decode($resumptionToken);
+                $resumArray=explode('_', $decodedresumption);
+                if($resumArray[0] == 'listIdent'){
+                    $page=intval($resumArray[1]);
+                    $start=$page * 100;
+                    if(($start + 100) < count($allIdentifiers)){
+                        $end = $start + 99;
+                        $newpage=$page+1;
+                        $cursor = $end + 1;
+                        $resumptionToken = base64_encode('listIdent_' . $newpage . '_token_' . $currdate);        
+                    }else{
+                        $end = count($allIdentifiers)-1;
+                        $cursor = $end + 1;
+                        $resumptionToken = '';
+                    }
+                }
+            }else{
+                $start = 0;
+                $end = 99;
+                $cursor = $end + 1;
+                $page = 1;
+                $resumptionToken = base64_encode('listIdent_' . $page . '_token_' . $currdate);
+            }
+        }else{
+            $start = 0;
+            $end = count($allIdentifiers)-1;
+            $resumptionToken = '';
+        }
+        $xmlidents = "<ListIdentifiers>";
+        $k=0;
+        for($i = $start; $i <= $end; $i++){
+            $identname = 'oai:nt-archive.gr:'.$metadataPrefix . ':' . $allIdentifiers[$i]['itemID'];
+            $identdate = str_replace(' ', 'T', $allIdentifiers[$i]['timestamp']).'Z';
+
+            $xmlidents .= '<header>
+                                <identifier>' . $identname . '</identifier>
+                                <datestamp>' . $identdate . '</datestamp>
+                                <setSpec>' . $allIdentifiers[$i]['setgroup'] . '</setSpec>
+                           </header>';
+            $k=$i;               
+        }
+
+        if($resumptionToken!=''){
+            $xmlidents .= '<resumptionToken cursor="' . $cursor . '" completeListSize="' . count($allIdentifiers) . '" >' . $resumptionToken . '</resumptionToken>';
+        }
+        $xmlidents .= "</ListIdentifiers>";
+        $xmlBody = $xmlidents;
+        
         break;
     default:
         $error = "badVerb";
