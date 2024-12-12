690459162f54d938e80ef242084bb8e247ef7e6c
nsidir <nsidir@athenarc.gr>
2024-12-12 13:01:52 +0200

    change url

--- config.dist.php
+++ config.dist.php
@@ -3,9 +3,9 @@
  * Configuration
  * *************/
 //$repo_url='https://www.nt-archive.gr/rdf';
-$repo_url='http://local.nt2.rdf';
+//$repo_url='http://local.nt2.rdf';
 //$handlerURL='https://hdl.handle.net/99999999999999';
-$handlerURL='http://194.177.217.106';
+$handlerURL='https://www.nt-archive.gr';
 $physicalCollectionPath="E:/Sites-IIS/NT_Collections";
 $NT_rdf='https://id.loc.gov/authorities/names/n90693693';
 
